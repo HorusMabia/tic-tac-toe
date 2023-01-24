67fa68b45da1d4f87e2444d5bb4b4664b85a5d87
HorusMabia <horus.mabia@gmail.com>
2023-01-24 13:56:31 +0000

    calculate winnner and no stealing square +status

--- src/index.js
+++ src/index.js
@@ -20,7 +20,9 @@ class Board extends React.Component {
   }
 
   handleClick(i) {
-    //this.squares[i]=""; <-- immutibility is better ... why exactly ? is it always the case ??
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     const squares = this.state.squares.slice();
     squares[i] = this.state.xIsNext ? "X" : "O";
 
@@ -40,7 +42,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "winner is" + winner;
+    } else {
+      status = "Next player:" + (this.state.xIsNext ? "X" : "O");
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -84,3 +92,23 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
